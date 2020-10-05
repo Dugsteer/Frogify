b9dafac953a381b18211691e8dd486aa97410985
Dugald Steer <dugsteer@gmail.com>
2020-10-05 13:24:58 +0200

    With sidebar js

---
+++ sidebar.js
@@ -0,0 +1,11 @@
+
+//Show ABOUT info
+const extra = document.getElementById('info');
+
+function show(){
+    info.classList.toggle('xxc');
+};
+
+function unshow(){
+    info.classList.remove('xxc');
+};
